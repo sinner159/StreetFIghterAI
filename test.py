50729ade404af71e72f8b5357836c1bc50c139b4
sinner159 <rsinner159@gmail.com>
2022-04-19 19:59:23 -0400

    init

---
+++ test.py
@@ -0,0 +1,16 @@
+import retro
+sfgame='StreetFighterIISpecialChampionEdition-Genesis'
+
+def main():
+    env = retro.make(game=sfgame)
+    obs = env.reset()
+    while True:
+        obs, rew, done, info = env.step(env.action_space.sample())
+        env.render()
+        if done:
+            obs = env.reset()
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
